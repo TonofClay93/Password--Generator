d43be9b9b8fb01c4f24ea280141912352e968aab
TonofClay93 <88801144+TonofClay93@users.noreply.github.com>
2021-09-07 22:35:25 -0400

    added if and prompts

--- Develop/script.js
+++ Develop/script.js
@@ -4,6 +4,28 @@
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
 
+//ask for password length function
+function generatePassword() {
+  var passLength = prompt("How long would you like the password to be? (Between 8-128) ");
+  //reset if they cant choose right amount.
+  if (passLength < 8 || passLength > 128) {
+    alert("Please enter a character length that is between 8 and 128");
+    return generatePassword ();
+  }
+  if (passLength >=8 && passLength <= 128) {
+    //prompts for password criteria
+    var speChar = confirm("Would you like to include special characters?");
+    var num = confirm("Would you like to include numbers?");
+    var lowerChar = confirm("Would you like to include lowercase letters?");
+    var upperChar = confirm("Would you like to include uppercase characters?");
+    }
+    //if function to ensure user selects criteria
+    if (speChar===false && num===false && lowerChar===false && upperChar===false) {
+      alert("Please confirm a set of criteria!");
+      return generatePassword();
+    }
+    
+
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
